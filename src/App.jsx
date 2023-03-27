47680872e35f8e98b1642a14a5967784bde39b8d
Marckgit <marckgit@gmail.com>
2023-03-27 17:03:04 -0600

    adding bg and logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import logo from "./assets/logo.png";
+import home from "./assets/home.svg";
 
 function App() {
   return (
     <div className=" bg-gradient-to-t from-fondos-gradient_bottow to-fondos-gradient_top h-screen w-screen">
-      <div className=" w-20 mx-auto pt-6">
+      <div className=" w-20 mx-auto pt-6 pb-2">
         <img src={logo} alt="" />
+      </div>
+      <div className=" w-20 mx-auto pt-6 pb-2">
+        <img src={home} alt="" />
       </div>
     </div>
   );
