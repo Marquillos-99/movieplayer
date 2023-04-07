db1efd7a76cfb35aadb859052b7487a64ace7660
Marckgit <marckgit@gmail.com>
2023-04-06 20:05:58 -0600

    'nothing_important'

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import logo from "./assets/logo.png";
 import home from "./assets/home.svg";
 import Carousel from "./components/Carousel";
+import disney from "./assets/disney_letters.png";
+import pixar from "./assets/pixar.png";
+import marvel from "./assets/marvel.png";
+import national from "./assets/national.png";
+import star_wars from "./assets/star_wars.png";
 
 function App() {
   return (
@@ -12,21 +17,21 @@ function App() {
         <Carousel></Carousel>
       </div>
       <div className=" flex m-auto text-center my-4 mx-2 ">
-        <div className=" w-14 h-14 bg-gradient-to-t from-buttons-bottom to-buttons-top border-buttons-border rounded-md mr-2 border border-opacity-10">
-          hola
-        </div>
-        <div className=" w-14 h-14 bg-gradient-to-t from-buttons-bottom to-buttons-top border-buttons-border rounded-md mr-2 border border-opacity-10">
-          hi
-        </div>
-        <div className=" w-14 h-14 bg-gradient-to-t from-buttons-bottom to-buttons-top border-buttons-border rounded-md mr-2 border border-opacity-10">
-          p
-        </div>
-        <div className=" w-14 h-14 bg-gradient-to-t from-buttons-bottom to-buttons-top border-buttons-border rounded-md mr-2 border border-opacity-10">
-          d
-        </div>
-        <div className=" w-14 h-14 bg-gradient-to-t from-buttons-bottom to-buttons-top border-buttons-border rounded-md border border-opacity-10">
-          dd
-        </div>
+        <button className=" w-14 h-14 bg-gradient-to-t from-buttons-bottom to-buttons-top border-buttons-border rounded-md mr-2 border border-opacity-10">
+          <img src={disney} alt="" />
+        </button>
+        <button className=" w-14 h-14 bg-gradient-to-t from-buttons-bottom to-buttons-top border-buttons-border rounded-md mr-2 border border-opacity-10">
+          <img src={pixar} alt="" />
+        </button>
+        <button className=" w-14 h-14 bg-gradient-to-t from-buttons-bottom to-buttons-top border-buttons-border rounded-md mr-2 border border-opacity-10">
+          <img src={marvel} alt="" />
+        </button>
+        <button className=" w-14 h-14 bg-gradient-to-t from-buttons-bottom to-buttons-top border-buttons-border rounded-md mr-2 border border-opacity-10">
+          <img src={star_wars} alt="" />
+        </button>
+        <button className=" w-14 h-14 bg-gradient-to-t from-buttons-bottom to-buttons-top border-buttons-border rounded-md border border-opacity-10">
+          <img src={national} alt="" />
+        </button>
       </div>
       <div className=" w-20 mx-auto pt-6 pb-2">
         <img src={home} alt="" />
